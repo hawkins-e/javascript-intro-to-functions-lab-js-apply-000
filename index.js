900e3a1fe07a3cf8cd0ee44e0770db6b3767c815
Erica Hawkins <ehawk901@gmail.com>
2019-04-20 00:34:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,12 +18,3 @@ function sayHiToGrandma(string)
 {
   return "I love you,too.";
 }
-function sayHiToGrandma(string)
-{
-  return "I can't hear you!";
-}
-function sayHiToGrandma(string) 
-if (string === lowercase.toLowerCase) 
-{
-  return "YES INDEED!"
-}
